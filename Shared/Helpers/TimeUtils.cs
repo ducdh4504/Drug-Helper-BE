467dcf50a33c08717778aa96fe8ea523fce93c29
ducdh4504 <huuduc04052004@gmail.com>
2026-03-16 11:00:41 +0700

    Add files .sln và /Shared

---
+++ Shared/Helpers/TimeUtils.cs
@@ -0,0 +1,22 @@
+﻿namespace Shared.Helpers
+{
+    public static class TimeUtils
+    {
+        private static readonly TimeZoneInfo VietnamTimeZone = TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time");
+
+        public static DateTime GetVietnamNow()
+        {
+            return TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, VietnamTimeZone);
+        }
+
+        public static DateTime ConvertUtcToVietnam(DateTime utcTime)
+        {
+            if (utcTime.Kind != DateTimeKind.Utc)
+            {
+                throw new ArgumentException("Input must be UTC time", nameof(utcTime));
+            }
+
+            return TimeZoneInfo.ConvertTimeFromUtc(utcTime, VietnamTimeZone);
+        }
+    }
+}
